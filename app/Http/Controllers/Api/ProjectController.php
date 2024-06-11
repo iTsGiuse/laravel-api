ac33cf1c6920334a9e30b2c465254adbffadd692
iTsGiuse <giuse.amato@icloud.com>
2024-06-11 17:34:44 +0200

    feat: add api/slug

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -18,4 +18,22 @@ class ProjectController extends Controller
             'results' => $projects
         ]);
     }
+
+    public function show($slug) {
+        $projects = Project::where('slug', '=', $slug)->with('technologies', 'type')->first();
+        
+        if($projects) {
+            $data = [
+                'success' => true,
+                'projects' => $projects
+            ];
+        } else {
+            $data = [
+                'success' => false,
+                'error' => 'No project found with this slug'
+            ];
+        }
+
+        return response()->json($data);
+    }
 }
